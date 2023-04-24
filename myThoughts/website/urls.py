b67908fe5d1ca408e65d202cb0d15f5b3a40e0fc
Errol Lagera <lagera.errol@gmail.com>
2023-04-24 20:28:13 +0800

    Added initial image as test

--- myThoughts/website/urls.py
+++ myThoughts/website/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_user, name='login'),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
