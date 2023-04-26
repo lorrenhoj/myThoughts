5c08a8860d1a4004363fe9fedf04281570af886c
Errol Lagera <lagera.errol@gmail.com>
2023-04-26 12:01:37 +0800

    Add register user

--- myThoughts/website/views.py
+++ myThoughts/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 
@@ -33,3 +34,25 @@ def logout_user(request):
     messages.success(request, 'You Have Been Logged Out...')
     return render(request, 'home.html', {})
 
+
+
+def register_user(request):
+	return render(request, 'register.html', {})
+
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+	return render(request, 'register.html', {'form':form})
